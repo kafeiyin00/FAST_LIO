165f62058579bb39572d484e9949206ff11e5041
zhaoxin <xinzhaodc@whu.edu.cn>
2025-06-10 16:42:35 +0800

    [modified:submap size settings]

--- src/laserMapping.cpp
+++ src/laserMapping.cpp
@@ -504,32 +504,33 @@ void publish_frame_world(const ros::Publisher & pubLaserCloudFull)
     /**************** save map ****************/
     /* 1. make sure you have enough memories
     /* 2. noted that pcd save will influence the real-time performences **/
-    // if (pcd_save_en)
-    // {
-    //     int size = feats_undistort->points.size();
-    //     PointCloudXYZI::Ptr laserCloudWorld( \
-    //                     new PointCloudXYZI(size, 1));
+    // save full map data
+    if (pcd_save_en)
+    {
+        int size = feats_undistort->points.size();
+        PointCloudXYZI::Ptr laserCloudWorld( \
+                        new PointCloudXYZI(size, 1));
 
-    //     for (int i = 0; i < size; i++)
-    //     {
-    //         RGBpointBodyToWorld(&feats_undistort->points[i], \
-    //                             &laserCloudWorld->points[i]);
-    //     }
-    //     *pcl_wait_save += *laserCloudWorld;
+        for (int i = 0; i < size; i++)
+        {
+            RGBpointBodyToWorld(&feats_undistort->points[i], \
+                                &laserCloudWorld->points[i]);
+        }
+        *pcl_wait_save += *laserCloudWorld;
 
-    //     static int scan_wait_num = 0;
-    //     scan_wait_num ++;
-    //     if (pcl_wait_save->size() > 0 && pcd_save_interval > 0  && scan_wait_num >= pcd_save_interval)
-    //     {
-    //         pcd_index ++;
-    //         string all_points_dir(string(string(ROOT_DIR) + "PCD/scans_") + to_string(pcd_index) + string(".pcd"));
-    //         pcl::PCDWriter pcd_writer;
-    //         cout << "current scan saved to /PCD/" << all_points_dir << endl;
-    //         pcd_writer.writeBinary(all_points_dir, *pcl_wait_save);
-    //         pcl_wait_save->clear();
-    //         scan_wait_num = 0;
-    //     }
-    // }
+        static int scan_wait_num = 0;
+        scan_wait_num ++;
+        // if (pcl_wait_save->size() > 0 && pcd_save_interval > 0  && scan_wait_num >= pcd_save_interval)
+        // {
+        //     pcd_index ++;
+        //     string all_points_dir(string(string(ROOT_DIR) + "PCD/scans_") + to_string(pcd_index) + string(".pcd"));
+        //     pcl::PCDWriter pcd_writer;
+        //     cout << "current scan saved to /PCD/" << all_points_dir << endl;
+        //     pcd_writer.writeBinary(all_points_dir, *pcl_wait_save);
+        //     pcl_wait_save->clear();
+        //     scan_wait_num = 0;
+        // }
+    }
 
     // jianping
     // save data for postprocessing
