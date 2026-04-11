26e4cf6e247e47a71ac25d7e4155f867b015457a
jianping <lijianping@whu.edu.cn>
2026-04-11 19:52:53 +0800

    sync version

--- scripts/start_aeos.sh
+++ scripts/start_aeos.sh
@@ -3,11 +3,16 @@
 xterm -e "source /opt/ros/noetic/setup.bash; \
                 source \$HOME/workspace/ws_driver/devel/setup.bash; \
                 roslaunch livox_ros_driver2 msg_MID360.launch" &
-                
+sleep 1                
+# xterm -e "source /opt/ros/noetic/setup.bash; \
+#                 source \$HOME/workspace/ws_slam/devel/setup.bash; \
+#                 cd \$HOME/workspace/ws_slam/src/FAST_LIO/scripts; \
+#                  python3 rotor_control_start_zero.py " &
+sleep 1
+
 xterm -e "source /opt/ros/noetic/setup.bash; \
                 source \$HOME/workspace/ws_slam/devel/setup.bash; \
-                cd \$HOME/workspace/ws_slam/src/FAST_LIO/scripts; \
-                 python3 rotor_control_start_zero.py " &
+                roslaunch motor_tf_utility rotor_control.launch" &
                  
                  
 sleep 5
@@ -29,11 +34,11 @@ xterm -e "source /opt/ros/noetic/setup.bash; \
                 roslaunch motor_tf_utility pointcloud_crop.launch" &
 
 # 等待3秒
-sleep 5
-xterm -e "source /opt/ros/noetic/setup.bash; \
-                source \$HOME/workspace/ws_slam/devel/setup.bash; \
-                cd \$HOME/workspace/ws_slam/src/FAST_LIO/scripts; \
-                 python3 rotor_control_start.py " &
+# sleep 5
+# xterm -e "source /opt/ros/noetic/setup.bash; \
+#                 source \$HOME/workspace/ws_slam/devel/setup.bash; \
+#                 cd \$HOME/workspace/ws_slam/src/FAST_LIO/scripts; \
+#                  python3 rotor_control_start.py " &
 sleep 1
 xterm -e "source /opt/ros/noetic/setup.bash; \
                  roslaunch mavros px4.launch fcu_url:=/dev/ttyACM_px4:921600 " &
